97019b7e72bd9b271c1d5c4b6309883fa5355f34
yusufceng <60972286+yusufceng@users.noreply.github.com>
2025-07-03 22:33:44 +0300

    Update start-camunda.sh

--- distro/tomcat/assembly/src/start-camunda.sh
+++ distro/tomcat/assembly/src/start-camunda.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Veritabanı bağlantı bilgilerini environment variable'lardan al
-export DB_DRIVER=${DB_DRIVER:-org.postgresql.Driver}
-export DB_URL=${DB_URL:-jdbc:postgresql://localhost:5432/camunda}
-export DB_USERNAME=${DB_USERNAME:-camunda}
-export DB_PASSWORD=${DB_PASSWORD:-camunda}
-export DB_SCHEMA_UPDATE=${DB_SCHEMA_UPDATE:-true}
-export AUTH_ENABLED=${AUTH_ENABLED:-true}
-export DB_CONN_MAXACTIVE=${DB_CONN_MAXACTIVE:-20}
-export DB_CONN_MINIDLE=${DB_CONN_MINIDLE:-5}
+# Environment variable'ları export et (varsayılan değerler olmadan)
+export DB_DRIVER
+export DB_URL
+export DB_USERNAME
+export DB_PASSWORD
+export DB_SCHEMA_UPDATE
+export AUTH_ENABLED
+export DB_CONN_MAXACTIVE
+export DB_CONN_MINIDLE
 
 # bpm-platform.xml dosyasını güncelle
 envsubst < /tmp/bpm-platform.xml.template > "$CATALINA_HOME/conf/bpm-platform.xml"
